bcbe18f507b06c89131516a584b33ee5318a1618
YangMH <fastyangmh@gmail.com>
2021-11-30 17:30:27 +0800

    rewrite parse_kwargs in project_parameters.py. add some transforms for audio in data_preparation.py. add MyImageFolder and MyAudioFolder class in data_preparation.py. TODO: finish MyLightningDataModule.

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -2,7 +2,7 @@
 import argparse
 from typing import Any, Optional, Callable, Union, Tuple
 import numpy as np
-from torchvision.datasets import MNIST, CIFAR10
+from torchvision.datasets import MNIST, CIFAR10, ImageFolder, DatasetFolder
 import random
 from torchaudio.datasets import SPEECHCOMMANDS
 from pathlib import Path
@@ -14,6 +14,9 @@ import torchaudio
 import torch.nn as nn
 import torch.nn.functional as F
 from torchaudio.functional import lowpass_biquad, highpass_biquad
+from PIL import Image
+from pytorch_lightning import LightningDataModule
+import torch
 
 
 #def
@@ -36,9 +39,15 @@ def parse_transforms(transforms_config):
                 transforms_dict[stage].append(eval('{}()'.format(name)))
             else:
                 if type(value) is dict:
-                    # TODO: if b is a string, an error will occur, and the type of b needs to be checked.
-                    value = ['{}={}'.format(a, b) for a, b in value.items()]
-                    value = ','.join(value)
+                    transform_arguments = []
+                    for a, b in value.items():
+                        if type(b) is str:
+                            arg = '{}="{}"'.format(a, b)
+                        else:
+                            arg = '{}={}'.format(a, b)
+                        transform_arguments.append(arg)
+                    transform_arguments = ','.join(transform_arguments)
+                    value = transform_arguments
                 transforms_dict[stage].append(
                     eval('{}({})'.format(name, value)))
         transforms_dict[stage] = torchvision.transforms.Compose(
@@ -47,6 +56,65 @@ def parse_transforms(transforms_config):
 
 
 #class
+
+
+class DigitalFilter(nn.Module):
+    def __init__(self, filter_type, sample_rate, cutoff_freq) -> None:
+        super().__init__()
+        assert filter_type in [
+            'bandpass', 'lowpass', 'highpass', None
+        ], 'please check the filter type is correct.\nfilter_type: {}\nvalid: {}'.format(
+            filter_type, ['bandpass', 'lowpass', 'highpass', None])
+        if type(cutoff_freq) != list:
+            cutoff_freq = [cutoff_freq]
+        cutoff_freq = np.array(cutoff_freq)
+        # check if the cutoff frequency satisfied Nyquist theorem
+        assert not any(
+            cutoff_freq / (sample_rate * 0.5) > 1
+        ), 'please check the cutoff frequency. the cutoff frequency value is {}.'.format(
+            cutoff_freq)
+        self.filter_type = filter_type
+        self.sample_rate = sample_rate
+        self.cutoff_freq = cutoff_freq
+
+    def __call__(self, waveform):
+        if self.filter_type is None or self.filter_type == 'None':
+            return waveform
+        elif self.filter_type == 'bandpass':
+            waveform = lowpass_biquad(waveform=waveform,
+                                      sample_rate=self.sample_rate,
+                                      cutoff_freq=max(self.cutoff_freq))
+            waveform = highpass_biquad(waveform=waveform,
+                                       sample_rate=self.sample_rate,
+                                       cutoff_freq=min(self.cutoff_freq))
+        elif self.filter_type == 'lowpass':
+            waveform = lowpass_biquad(waveform=waveform,
+                                      sample_rate=self.sample_rate,
+                                      cutoff_freq=max(self.cutoff_freq))
+        elif self.filter_type == 'highpass':
+            waveform = highpass_biquad(waveform=waveform,
+                                       sample_rate=self.sample_rate,
+                                       cutoff_freq=min(self.cutoff_freq))
+        return waveform
+
+
+class PadWaveform(nn.Module):
+    def __init__(self, max_waveform_length) -> None:
+        super().__init__()
+        self.max_waveform_length = max_waveform_length
+
+    def forward(self, waveform):
+        # the dimension of waveform is (channels, length)
+        channels, length = waveform.shape
+        diff = self.max_waveform_length - length
+        if diff >= 0:
+            pad = (int(np.ceil(diff / 2)), int(np.floor(diff / 2)))
+            waveform = F.pad(input=waveform, pad=pad)
+        else:
+            waveform = waveform[:, :self.max_waveform_length]
+        return waveform
+
+
 class OneHotEncoder:
     def __init__(self, num_classes) -> None:
         self.num_classes = num_classes
@@ -146,67 +214,64 @@ class MySPEECHCOMMANDS(SPEECHCOMMANDS):
         return sample, target
 
 
-class DigitalFilter(nn.Module):
-    def __init__(self, filter_type, sample_rate, cutoff_freq) -> None:
-        super().__init__()
-        filter_type = {
-            0: None,
-            1: 'bandpass',
-            2: 'lowpass',
-            3: 'highpass'
-        }[filter_type]
-        assert filter_type in [
-            'bandpass', 'lowpass', 'highpass', None
-        ], 'please check the filter type is correct.\nfilter_type: {}\nvalid: {}'.format(
-            filter_type, ['bandpass', 'lowpass', 'highpass', None])
-        if type(cutoff_freq) != list:
-            cutoff_freq = [cutoff_freq]
-        cutoff_freq = np.array(cutoff_freq)
-        # check if the cutoff frequency satisfied Nyquist theorem
-        assert not any(
-            cutoff_freq / (sample_rate * 0.5) > 1
-        ), 'please check the cutoff frequency. the cutoff frequency value is {}.'.format(
-            cutoff_freq)
-        self.filter_type = filter_type
-        self.sample_rate = sample_rate
-        self.cutoff_freq = cutoff_freq
+class MyImageFolder(ImageFolder):
+    def __init__(self,
+                 root: str,
+                 transform: Optional[Callable] = None,
+                 target_transform: Optional[Callable] = None):
+        super().__init__(root,
+                         transform=transform,
+                         target_transform=target_transform,
+                         loader=Image.open)
 
-    def __call__(self, waveform):
-        if self.filter_type is None or self.filter_type == 'None':
-            return waveform
-        elif self.filter_type == 'bandpass':
-            waveform = lowpass_biquad(waveform=waveform,
-                                      sample_rate=self.sample_rate,
-                                      cutoff_freq=max(self.cutoff_freq))
-            waveform = highpass_biquad(waveform=waveform,
-                                       sample_rate=self.sample_rate,
-                                       cutoff_freq=min(self.cutoff_freq))
-        elif self.filter_type == 'lowpass':
-            waveform = lowpass_biquad(waveform=waveform,
-                                      sample_rate=self.sample_rate,
-                                      cutoff_freq=max(self.cutoff_freq))
-        elif self.filter_type == 'highpass':
-            waveform = highpass_biquad(waveform=waveform,
-                                       sample_rate=self.sample_rate,
-                                       cutoff_freq=min(self.cutoff_freq))
-        return waveform
+    def decrease_samples(self, max_samples):
+        if max_samples is not None:
+            self.samples = random.sample(population=self.samples,
+                                         k=max_samples)
 
 
-class PadWaveform(nn.Module):
-    def __init__(self, max_waveform_length) -> None:
-        super().__init__()
-        self.max_waveform_length = max_waveform_length
+class MyAudioFolder(DatasetFolder):
+    def __init__(self,
+                 root: str,
+                 loader: Callable[[str], Any],
+                 transform: Optional[Callable] = None,
+                 target_transform: Optional[Callable] = None) -> None:
+        super().__init__(root,
+                         loader=loader,
+                         extensions=('.wav'),
+                         transform=transform,
+                         target_transform=target_transform)
 
-    def forward(self, waveform):
-        # the dimension of waveform is (channels, length)
-        channels, length = waveform.shape
-        diff = self.max_waveform_length - length
-        if diff >= 0:
-            pad = (int(np.ceil(diff / 2)), int(np.floor(diff / 2)))
-            waveform = F.pad(input=waveform, pad=pad)
-        else:
-            waveform = waveform[:, :self.max_waveform_length]
-        return waveform
+    def decrease_samples(self, max_samples):
+        if max_samples is not None:
+            self.samples = random.sample(population=self.samples,
+                                         k=max_samples)
+
+
+#TODO: finish MyLightningDataModule.
+class MyLightningDataModule(LightningDataModule):
+    def __init__(self, project_parameters, transforms_dict,
+                 target_transforms_dict):
+        self.root = project_parameters.root
+        assert project_parameters.predefined_dataset in [
+            'MNIST', 'CIFAR10', 'SPEECHCOMMANDS', None
+        ], 'please check the predefined_dataset argument.\npredefined_dataset: {}\nvalid: {}'.format(
+            project_parameters.predefined_dataset,
+            ['MNIST', 'CIFAR10', 'SPEECHCOMMANDS', None])
+        self.predefined_dataset = project_parameters.predefined_dataset
+        self.classes = project_parameters.classes
+        self.max_samples = project_parameters.max_samples
+        self.batch_size = project_parameters.batch_size
+        self.num_workers = project_parameters.num_workers
+        assert project_parameters.device in [
+            'cpu', 'cuda'
+        ], 'please check the device argument.\ndevice: {}\nvalid: {}'.format(
+            project_parameters.device, ['cpu', 'cuda'])
+        self.pin_memory = project_parameters.device == 'cuda' and torch.cuda.is_available(
+        )
+        self.transforms_dict = transforms_dict
+        self.target_transforms_dict = target_transforms_dict
+        self.val_size = 0.2
 
 
 if __name__ == '__main__':
@@ -224,14 +289,26 @@ if __name__ == '__main__':
             },
             'val': {
                 'Resize': [224, 224],
+                'ColorJitter': None,
+                'RandomRotation': 90,
+                'RandomHorizontalFlip': None,
+                'RandomVerticalFlip': None,
                 'ToTensor': None
             },
             'test': {
                 'Resize': [224, 224],
+                'ColorJitter': None,
+                'RandomRotation': 90,
+                'RandomHorizontalFlip': None,
+                'RandomVerticalFlip': None,
                 'ToTensor': None
             },
             'predict': {
                 'Resize': [224, 224],
+                'ColorJitter': None,
+                'RandomRotation': 90,
+                'RandomHorizontalFlip': None,
+                'RandomVerticalFlip': None,
                 'ToTensor': None
             }
         }
@@ -239,5 +316,3 @@ if __name__ == '__main__':
     project_parameters = argparse.Namespace(**project_parameters)
 
     #
-    transforms_dict = parse_transforms(
-        transforms_config=project_parameters.transforms_config)
