0821e59e9096be5da5ce64938c74932ae4b08d96
YangMH <fastyangmh@gmail.com>
2021-12-01 12:32:52 +0800

    add parse_target_transforms function. add AudioLoader. add BaseLightningDataModule. add ImageLightningDataModule, AudioLightningDataModule.

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -1,6 +1,6 @@
 #import
 import argparse
-from typing import Any, Optional, Callable, Union, Tuple
+from typing import Any, Optional, Callable, Union, Tuple, List, Dict
 import numpy as np
 from torchvision.datasets import MNIST, CIFAR10, ImageFolder, DatasetFolder
 import random
@@ -17,11 +17,14 @@ from torchaudio.functional import lowpass_biquad, highpass_biquad
 from PIL import Image
 from pytorch_lightning import LightningDataModule
 import torch
+from os.path import join
+from torch.utils.data import random_split, DataLoader
+from os import makedirs
 
 
 #def
 def parse_transforms(transforms_config):
-    if transforms_config == 'None' or transforms_config is None:
+    if transforms_config is None:
         return {'train': None, 'val': None, 'test': None, 'predict': None}
     transforms_dict = defaultdict(list)
     for stage in transforms_config.keys():
@@ -55,7 +58,48 @@ def parse_transforms(transforms_config):
     return transforms_dict
 
 
+def parse_target_transforms(target_transforms_config, classes):
+    if target_transforms_config is None:
+        return {'train': None, 'val': None, 'test': None, 'predict': None}
+    target_transforms_dict = {}
+    for stage in target_transforms_config.keys():
+        if target_transforms_config[stage] is None:
+            target_transforms_dict[stage] = None
+            continue
+        for name, value in target_transforms_config[stage].items():
+            if type(value) is dict:
+                target_transform_arguments = []
+                for a, b in value.items():
+                    if a == 'num_classes' and b is None:
+                        b = len(classes)
+                    arg = '{}={}'.format(a, b)
+                    target_transform_arguments.append(arg)
+                target_transform_arguments = ','.join(
+                    target_transform_arguments)
+                value = target_transform_arguments
+            target_transforms_dict[stage] = eval('{}({})'.format(name, value))
+    return target_transforms_dict
+
+
+#TODO: finish create_data_module.
+def create_data_module(project_parameters):
+    transforms_dict = parse_transforms(
+        transforms_config=project_parameters.transforms_config)
+    target_transforms_dict = parse_target_transforms(
+        project_parameters.target_transforms_config)
+    pass
+
+
 #class
+class AudioLoader:
+    def __init__(self, sample_rate) -> None:
+        self.sample_rate = sample_rate
+
+    def __call__(self, path) -> Any:
+        sample, sample_rate = torchaudio.load(path)
+        assert self.sample_rate == sample_rate, 'please check the sample_rate argument.\nsample_rate: {}\nvalid: {}'.format(
+            self.sample_rate, sample_rate)
+        return sample
 
 
 class DigitalFilter(nn.Module):
@@ -63,7 +107,7 @@ class DigitalFilter(nn.Module):
         super().__init__()
         assert filter_type in [
             'bandpass', 'lowpass', 'highpass', None
-        ], 'please check the filter type is correct.\nfilter_type: {}\nvalid: {}'.format(
+        ], 'please check the filter_type argument.\nfilter_type: {}\nvalid: {}'.format(
             filter_type, ['bandpass', 'lowpass', 'highpass', None])
         if type(cutoff_freq) != list:
             cutoff_freq = [cutoff_freq]
@@ -71,8 +115,8 @@ class DigitalFilter(nn.Module):
         # check if the cutoff frequency satisfied Nyquist theorem
         assert not any(
             cutoff_freq / (sample_rate * 0.5) > 1
-        ), 'please check the cutoff frequency. the cutoff frequency value is {}.'.format(
-            cutoff_freq)
+        ), 'please check the cutoff_freq argument.\ncutoff_freq: {}\nvalid: {}'.format(
+            cutoff_freq, [1, sample_rate // 2])
         self.filter_type = filter_type
         self.sample_rate = sample_rate
         self.cutoff_freq = cutoff_freq
@@ -249,7 +293,7 @@ class MyAudioFolder(DatasetFolder):
 
 
 #TODO: finish MyLightningDataModule.
-class MyLightningDataModule(LightningDataModule):
+class BaseLightningDataModule(LightningDataModule):
     def __init__(self, project_parameters, transforms_dict,
                  target_transforms_dict):
         self.root = project_parameters.root
@@ -272,6 +316,178 @@ class MyLightningDataModule(LightningDataModule):
         self.transforms_dict = transforms_dict
         self.target_transforms_dict = target_transforms_dict
         self.val_size = 0.2
+
+    def train_dataloader(
+            self
+    ) -> Union[DataLoader, List[DataLoader], Dict[str, DataLoader]]:
+        return DataLoader(dataset=self.train_dataset,
+                          batch_size=self.batch_size,
+                          shuffle=True,
+                          num_workers=self.num_workers,
+                          pin_memory=self.pin_memory)
+
+    def val_dataloader(self) -> Union[DataLoader, List[DataLoader]]:
+        return DataLoader(dataset=self.val_dataset,
+                          batch_size=self.batch_size,
+                          shuffle=False,
+                          num_workers=self.num_workers,
+                          pin_memory=self.pin_memory)
+
+    def test_dataloader(self) -> Union[DataLoader, List[DataLoader]]:
+        return DataLoader(dataset=self.test_dataset,
+                          batch_size=self.batch_size,
+                          shuffle=False,
+                          num_workers=self.num_workers,
+                          pin_memory=self.pin_memory)
+
+
+class ImageLightningDataModule(BaseLightningDataModule):
+    def __init__(self, project_parameters, transforms_dict,
+                 target_transforms_dict):
+        super().__init__(project_parameters, transforms_dict,
+                         target_transforms_dict)
+
+    def prepare_data(self) -> None:
+        # download predefined dataset
+        root = join(self.root, self.predefined_dataset)
+        eval(
+            'My{}(root=root, train=True, transform=None, target_transform=None, download=True)'
+            .format(self.predefined_dataset))
+        eval(
+            'My{}(root=root, train=False, transform=None, target_transform=None, download=True)'
+            .format(self.predefined_dataset))
+
+    def setup(self, stage: Optional[str] = None) -> None:
+        if stage == 'fit' or stage is None:
+            if self.predefined_dataset in ['MNIST', 'CIFAR10']:
+                # load predefined dataset
+                train_dataset = eval(
+                    'My{}(root=root, train=True, transform=self.transforms_dict["train"], target_transform=self.target_transforms_dict["train"], download=True)'
+                    .format(self.predefined_dataset))
+                assert self.classes == train_dataset.classes, 'please check the classes argument.\nclasses: {}\nvalid: {}'.format(
+                    self.classes, train_dataset.classes)
+                train_dataset.decrease_samples(max_samples=self.max_samples)
+                lengths = (np.array([1 - self.val_size, self.val_size]) *
+                           len(train_dataset)).astype(int)
+                self.train_dataset, self.val_dataset = random_split(
+                    dataset=train_dataset, lengths=lengths)
+            else:
+                # load dataset from root
+                self.train_dataset = MyImageFolder(
+                    root=join(self.root, 'train'),
+                    transform=self.transforms_dict['train'],
+                    target_transform=self.target_transforms_dict['train'])
+                assert self.classes == self.train_dataset.classes, 'please check the classes argument.\nclasses: {}\nvalid: {}'.format(
+                    self.classes, self.train_dataset.classes)
+                self.train_dataset.decrease_samples(
+                    max_samples=self.max_samples)
+                self.val_dataset = MyImageFolder(
+                    root=join(self.root, 'val'),
+                    transform=self.transforms_dict['val'],
+                    target_transform=self.target_transforms_dict['val'])
+                self.val_dataset.decrease_samples(max_samples=self.max_samples)
+
+        if stage == 'test' or stage is None:
+            if self.predefined_dataset in ['MNIST', 'CIFAR10']:
+                # load predefined dataset
+                root = join(self.root, self.predefined_dataset)
+                self.test_dataset = eval(
+                    'My{}(root=root, train=False, transform=self.transforms_dict["test"], target_transform=self.target_transforms_dict["test"], download=True)'
+                    .format(self.predefined_dataset))
+                self.test_dataset.decrease_samples(
+                    max_samples=self.max_samples)
+            else:
+                # load dataset from root
+                self.test_dataset = MyImageFolder(
+                    root=join(self.root, 'test'),
+                    transform=self.transforms_dict['test'],
+                    target_transform=self.target_transforms_dict['test'])
+                self.test_dataset.decrease_samples(
+                    max_samples=self.max_samples)
+
+
+class AudioLightningDataModule(BaseLightningDataModule):
+    def __init__(self, project_parameters, transforms_dict,
+                 target_transforms_dict):
+        super().__init__(project_parameters, transforms_dict,
+                         target_transforms_dict)
+        self.loader = AudioLoader(sample_rate=project_parameters.sample_rate)
+
+    def prepare_data(self) -> None:
+        # download predefined dataset
+        root = join(self.root, self.predefined_dataset)
+        makedirs(name=root, exist_ok=True)
+        MySPEECHCOMMANDS(root=root,
+                         loader=None,
+                         transform=None,
+                         target_transform=None,
+                         download=True,
+                         subset=None)
+
+    def setup(self, stage: Optional[str] = None) -> None:
+        if stage == 'fit' or stage is None:
+            if self.predefined_dataset in ['SPEECHCOMMANDS']:
+                # load predefined dataset
+                root = join(self.root, self.predefined_dataset)
+                self.train_dataset = MySPEECHCOMMANDS(
+                    root=root,
+                    loader=self.loader,
+                    transform=self.transforms_dict['train'],
+                    target_transform=self.target_transforms_dict['train'],
+                    download=True,
+                    subset='training')
+                assert self.classes == self.train_dataset, 'please check the classes argument.\nclasses: {}\nvalid: {}'.format(
+                    self.classes, self.train_dataset)
+                self.train_dataset.decrease_samples(
+                    max_samples=self.max_samples)
+                self.val_dataset = MySPEECHCOMMANDS(
+                    root=root,
+                    loader=self.loader,
+                    transform=self.transforms_dict['val'],
+                    target_transform=self.target_transforms_dict['val'],
+                    download=True,
+                    subset='validation')
+                self.val_dataset.decrease_samples(max_samples=self.max_samples)
+            else:
+                # load dataset from root
+                self.train_dataset = MyAudioFolder(
+                    root=join(self.root, 'train'),
+                    loader=self.loader,
+                    transform=self.transforms_dict['train'],
+                    target_transform=self.target_transforms_dict['train'])
+                assert self.classes == self.train_dataset, 'please check the classes argument.\nclasses: {}\nvalid: {}'.format(
+                    self.classes, self.train_dataset)
+                self.train_dataset.decrease_samples(
+                    max_samples=self.max_samples)
+                self.val_dataset = MyAudioFolder(
+                    root=join(self.root, 'val'),
+                    loader=self.loader,
+                    transform=self.transforms_dict['val'],
+                    target_transform=self.target_transforms_dict['val'])
+                self.val_dataset.decrease_samples(max_samples=self.max_samples)
+
+        if stage == 'test' or stage is None:
+            if self.predefined_dataset in ['SPEECHCOMMANDS']:
+                # load predefined dataset
+                root = join(self.root, self.predefined_dataset)
+                self.test_dataset = MySPEECHCOMMANDS(
+                    root=root,
+                    loader=self.loader,
+                    transform=self.transforms_dict['test'],
+                    target_transform=self.target_transforms_dict['test'],
+                    download=True,
+                    subset='testing')
+                self.test_dataset.decrease_samples(
+                    max_samples=self.max_samples)
+            else:
+                # load dataset from root
+                self.test_dataset = MyAudioFolder(
+                    root=join(self.root, 'test'),
+                    loader=self.loader,
+                    transform=self.transforms_dict['test'],
+                    target_transform=self.target_transforms_dict['test'])
+                self.test_dataset.decrease_samples(
+                    max_samples=self.max_samples)
 
 
 if __name__ == '__main__':
@@ -310,6 +526,24 @@ if __name__ == '__main__':
                 'RandomHorizontalFlip': None,
                 'RandomVerticalFlip': None,
                 'ToTensor': None
+            }
+        },
+        'target_transforms_config': {
+            'train': {
+                'LabelSmoothing': {
+                    'alpha': 0.2,
+                    'num_classes': None
+                }
+            },
+            'val': {
+                'OneHotEncoder': {
+                    'num_classes': None
+                }
+            },
+            'test': {
+                'OneHotEncoder': {
+                    'num_classes': None
+                }
             }
         }
     }
